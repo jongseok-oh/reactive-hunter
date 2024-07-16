60b1f1b2fe809f89bc54c3aa6c02073b45f8993b
jongseok-oh <hynb14@gmail.com>
2024-07-16 17:04:10 +0900

    project init

---
+++ src/main/java/com/reactive/hunter/jongking/JongkingApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.hunter.jongking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JongkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JongkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/hunter/jongking/JongkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.hunter.jongking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JongkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
